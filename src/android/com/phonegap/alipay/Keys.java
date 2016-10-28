9ad5b7bb415a96d192a2bb7b7ecf06dc94f4c769
gonnavis <gonnavis@163.com>
2016-10-28 10:21:16 +0800

    a

--- src/android/com/phonegap/alipay/Keys.java
+++ src/android/com/phonegap/alipay/Keys.java
@@ -27,6 +27,6 @@ public final class Keys {
 	public static final String PRIVATE = "";
    
    //支付宝默认公钥 ，请勿修改
-	public static final String PUBLIC = "";
+	public static final String PUBLIC = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCnxj/9qwVfgoUh/y2W89L6BkRAFljhNhgPdyPuBV64bfQNN1PjbCzkIM6qRdKBoLPXmKKMiFYnkd6rAoprih3/PrQEB/VsW8OoM8fxn67UDYuyBTqA23MML9q1+ilIZwBC2AQ2UBVOrFXfFl75p6/B5KsiNG9zpgmLCUYuLkxpLQIDAQAB";
 
 }
